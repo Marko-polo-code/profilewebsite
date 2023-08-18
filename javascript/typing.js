acbc65699dd7b091d4d4ee6df537e6968d0f5338
Mark Muller <mark.muller.w@gmail.com>
2023-08-18 15:07:26 +0200

    a delay fix

--- javascript/typing.js
+++ javascript/typing.js
@@ -1,12 +1,12 @@
 const textElement = document.getElementById('changingText');
 const newTexts = [
   "I am a Fullstack engineer.",
-  "I make websites and apps.",
+  "I can make websites and apps."
 ];
 let currentIndex = 0;
 let charIndex = 0;
 let isDeleting = false;
-let delay = 1000; // Pause duration in milliseconds
+const typingSpeed = 100; // Typing animation speed in milliseconds
 
 function typeText() {
   const currentText = newTexts[currentIndex];
@@ -14,19 +14,18 @@ function typeText() {
   if (!isDeleting && charIndex < currentText.length) {
     textElement.textContent = currentText.slice(0, charIndex + 1);
     charIndex++;
-  } else if (isDeleting && charIndex > 0) {
-    textElement.textContent = currentText.slice(0, charIndex - 1);
+  } else if (isDeleting && charIndex >= 0) {
+    textElement.textContent = currentText.slice(0, charIndex);
     charIndex--;
   } else if (!isDeleting) {
     isDeleting = true;
-    delay = 1000; // Pause before deleting
   } else {
     isDeleting = false;
-    delay = 1000; // Pause before next typing
     currentIndex = (currentIndex + 1) % newTexts.length;
   }
 
-  setTimeout(typeText, isDeleting ? delay : 100);
+  const delay = isDeleting ? typingSpeed / 2 : typingSpeed; // Pause between typing and deleting
+  setTimeout(typeText, delay);
 }
 
-setTimeout(typeText, 100); // Start the typing animation
+setTimeout(typeText, typingSpeed); // Start the typing animation
