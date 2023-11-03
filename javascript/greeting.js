f14800d995ad34bc6f3efc0db5d0f8f3665313d2
Mark Muller <mark.muller.w@gmail.com>
2023-11-03 18:31:51 +0100

    fixed some languages

--- javascript/greeting.js
+++ javascript/greeting.js
@@ -1,13 +1,22 @@
 
 function greeting() {
   let count = -1; // Start at -1 to begin with the first greeting
-  const wordsArray = ["Hola", "Salut", "Hallo", "你好", "Salam", "Konnichiwa", "Asalaam alaikum", "Hi"];
+  const wordsArray = [
+    "Hola, mi nombre es Mark!",
+    "Salut, je m'appelle Mark!",
+    "Hallo, mein Name ist Mark!",
+    "你好，我叫Mark!",
+    "Salam, nama saya Mark!",
+    "こんにちは、私の名前はMarkです!",
+    "السلام عليكم، اسمي مارك!",
+    "Hi, my name is Mark!"
+  ];
   const greetingElement = document.getElementById("greeting");
 
   setInterval(function () {
     count++;
     greetingElement.innerHTML = wordsArray[count % wordsArray.length];
-  }, 4000);
+  }, 8000);
 }
 
 greeting();
