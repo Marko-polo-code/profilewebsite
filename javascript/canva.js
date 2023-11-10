5a3a97f23cb1d3b9bff29698a2ec487659032aa5
Mark Muller <mark.muller.w@gmail.com>
2023-11-10 19:36:48 +0100

    matched the background color for the canvas with website

--- javascript/canva.js
+++ javascript/canva.js
@@ -92,7 +92,7 @@ while (balls.length < 25) {
 }
 
 function loop() {
-   ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+   ctx.fillStyle = 'rgb(28, 28, 28, 0.25)';
    ctx.fillRect(0, 0,  width, height);
 
    for (const ball of balls) {
